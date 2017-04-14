e8865e244f9a8ad8a1111ef36132fd23e48fb1fd
Tighe Kuykendall <tighe@buoy.net>
2017-04-14 10:40:31 -0400

    no message

--- lego_control.py
+++ lego_control.py
@@ -4,32 +4,50 @@ import usb.core
 import usb.util
 from time import sleep
 
+# Toypad initiliztion string
+#
 TOYPAD_INIT = [0x55, 0x0f, 0xb0, 0x01, 0x28, 0x63, 0x29, 0x20, 0x4c, 0x45, 0x47, 0x4f, 0x20, 0x32, 0x30, 0x31, 0x34, 0xf7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
 
+# Toypad colors
+#
 OFF   = [0,0,0]
 RED   = [255,0,0]
 GREEN = [0,255,0]
 BLUE  = [0,0,255]
 
+# Toypad locations
+#
 ALL_PADS   = 0
 CENTER_PAD = 1
 LEFT_PAD   = 2
 RIGHT_PAD  = 3
 
+# Tag actions
+#
+TAG_INSERTED = 0
+TAG_REMOVED  = 1
+
+# Tag UIDs
+#
+uidDarthVader = (4, 161, 158, 210, 227, 64 , 128) # Disney Infinity 3.0
+
+
 def init_usb():
     global dev
 
+    # Define the Toypad device
+    #
     dev = usb.core.find(idVendor=0x0e6f, idProduct=0x0241)
 
+    # Look for the device and initilize
+    #
     if dev is None:
         print 'Device not found'
     else:
-        print 'Found the device' + usb.util.get_string(dev, dev.iProduct)
+        print 'Found the device:  ' + usb.util.get_string(dev, dev.iProduct)
         
         if dev.is_kernel_driver_active(0):
             dev.detach_kernel_driver(0)
-
-        print usb.util.get_string(dev, dev.iProduct)
 
         dev.set_configuration()
         dev.write(1,TOYPAD_INIT)
@@ -37,7 +55,8 @@ def init_usb():
 
 def send_command(dev,command):
 
-    # calculate checksum
+    # Calculate checksum for the message
+    #
     checksum = 0
     for word in command:
         checksum = checksum + word
@@ -45,45 +64,71 @@ def send_command(dev,command):
             checksum -= 256
     message = command+[checksum]
 
-    # pad message
+    # Toypad message
+    #
     while(len(message) < 32):
         message.append(0x00)
 
-    # send message
+    # Send message to Toypad
+    #
     dev.write(1, message)
 
     return
 
-def switch_pad(pad, colour):
-    send_command(dev,[0x55, 0x06, 0xc0, 0x02, pad, colour[0], colour[1], colour[2],])
+def switch_pad_color(pad, color):
+    
+    # Switch pad color
+    #
+    send_command(dev,[0x55, 0x06, 0xc0, 0x02, pad, color[0], color[1], color[2],])
     return
 
 
-try:
-    print ("Starting up...")
-    init_usb()
-    switch_pad(ALL_PADS,BLUE)
-    sleep(1)
-    switch_pad(LEFT_PAD,GREEN)
-    sleep(1)
-    switch_pad(RIGHT_PAD,RED)
-    sleep(1)
-    switch_pad(CENTER_PAD,OFF)
-    sleep(.5)
-    switch_pad(CENTER_PAD,BLUE)
-    sleep(.5)
-    switch_pad(CENTER_PAD,OFF)
-    sleep(.5)
-    switch_pad(CENTER_PAD,BLUE)
-    sleep(1)
-    switch_pad(ALL_PADS,OFF)
-    print ("Done!")
+def main():
 
-except Exception, err:
-    print ("Exception:", str(err))
-    import traceback, sys
-    print ('-'*60)
-    traceback.print_exc(file=sys.stdout)
-    print ('-'*60)
+    print ("Starting up...")
     
+    # Initialize the Toypad
+    #
+    init_usb()
+    
+    # Start a loop looking for tags
+    #
+    if dev != None :
+        while True:
+            try:
+                in_packet = dev.read(0x81, 32, timeout = 10)
+                bytelist = list(in_packet)
+
+                if not bytelist:
+                    pass
+                elif bytelist[0] != 0x56: # NFC packets start with 0x56
+                    pass
+                else:
+                    print bytelist[6:13]
+                    pad_num = bytelist[2]
+                    uid_bytes = bytelist[6:13]
+                    match = uid_compare(uid_bytes, uidDarthVader)
+                    action = bytelist[5]
+                    if action == TAG_INSERTED :
+                        if match:
+                            # Darth Vader
+                            switch_pad_color(pad_num, RED)
+                        else:
+                            # some other tag
+                            switch_pad_color(pad_num, GREEN)
+                    else:
+                        # some tag removed
+                        switch_pad(pad_num, OFF)
+
+            except usb.USBError, err:
+                print ("Exception:", str(err))
+                pass
+
+        switch_pad(ALL_PADS,OFF)
+
+    return
+
+if __name__ == '__main__':
+    main()
+
 #End
